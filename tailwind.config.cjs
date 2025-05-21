4f7ae6b0323fc923b8722b945c12317ab6ac0d58
Dmitriy-Hrihor <dmytro.hryhor@gmail.com>
2025-05-21 20:28:46 +0200

    fix some

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,4 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -7,6 +6,5 @@ export default {
   theme: {
     extend: {},
   },
-  darkMode: 'class',
   plugins: [],
 }
