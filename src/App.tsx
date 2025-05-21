955c53f5e19eacb5a1a19e2946917f465d6200e1
Dmitriy-Hrihor <dmytro.hryhor@gmail.com>
2025-05-21 21:53:43 +0200

    add some

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import './App.css'
+ import './App.css'
+
 
 function App() {
 
   return (
     <>
-      <div className='bg-blue-500 h-screen flex items-center justify-center'>
-        <h1 className='text-white text-4xl font-bold'>Hello World</h1>
-        
-        <h1 className='ext-white text-4xl font-bold'>Tailwind sucsess</h1>
+      <div className='bg-red-200'>
+        <h1 className='text-red text-4xl font-bold'>Hello World</h1>
+        <h1 className='text-red text-4xl font-bold'>Tailwind sucsess</h1>
       </div>
     </>
   )
